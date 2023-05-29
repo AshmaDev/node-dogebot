70204d26107ec778745dcdf4906ca04f197391c7
AshmaDev <ashmadev@protonmail.com>
2023-05-29 21:10:59 +0200

    Add take profit func..

--- src/dogebot.ts
+++ src/dogebot.ts
@@ -3,7 +3,7 @@ import createBinance, {
     Binance,
     Account,
     AssetBalance,
-    OrderSide_LT,
+    OrderSide,
     OrderType,
     OrderFill,
     Order,
@@ -13,11 +13,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const MINIMUM_TRADE_VALUE: number = 10;
-const TRADE_SIDE: OrderSide_LT = "BUY";
 
 export interface ITradeConfig {
     enabled?: boolean;
     balancePercentage?: number;
+    timeToSell?: number;
     coin?: string;
     pairCoin?: string
 }
@@ -39,7 +39,7 @@ export class DogeBot {
     constructor(config: IDogeBotConfig) {
         this.twitterId = config.twitterId;
         this.allowReplies = config.allowReplies ?? false;
-        this.trade = { enabled: false, balancePercentage: 1, ...config.trade };
+        this.trade = { enabled: false, balancePercentage: 1, timeToSell: 5, ...config.trade };
 
         this.twitterClient = this.createTwitterClient();
 
@@ -72,13 +72,10 @@ export class DogeBot {
     };
 
     private async createOrder(): Promise<void> {
-        if (!this.binanceClient) return;
-
         const pairSymbol: string = `${this.trade.coin}${this.trade.pairCoin}`;
-        const priceList: { [index: string]: string } = await this.binanceClient!.prices({ symbol: pairSymbol });
-        const price: string = priceList[pairSymbol];
+        const price: string = await this.getCurrentPairPrice();
 
-        const accountInfo: Account = await this.binanceClient.accountInfo();
+        const accountInfo: Account = await this.binanceClient!.accountInfo();
         const pairCoinBalance: AssetBalance | undefined = accountInfo.balances.find((asset) => asset.asset === this.trade.pairCoin);
 
         if (!pairCoinBalance || parseFloat(pairCoinBalance.free) < MINIMUM_TRADE_VALUE) {
@@ -89,9 +86,9 @@ export class DogeBot {
         const tradeAmount: number = parseFloat(pairCoinBalance.free) * this.trade.balancePercentage!;
         const buyAmount: number = tradeAmount / parseFloat(price);
 
-        const order: Order = await this.binanceClient.order({
+        const order: Order = await this.binanceClient!.order({
             symbol: pairSymbol,
-            side: TRADE_SIDE,
+            side: OrderSide.BUY,
             quantity: buyAmount.toString(),
             type: OrderType.MARKET,
             recvWindow: 60000,
@@ -110,10 +107,67 @@ export class DogeBot {
 
             const avgPrice: number = price / order.fills.length;
 
-            console.log(`Bought: ${order.origQty} ${order.symbol}. \n\nAvg. price: ${avgPrice} (${quantity} ${order.symbol}). \n\nCommission fee: ${commission} (${order.fills[0].commissionAsset})`);
+            console.log(`Bought: ${order.origQty} ${order.symbol}. \n\n
+            Avg. price: ${avgPrice} (${quantity} ${order.symbol}). \n\n
+            Commission fee: ${commission} (${order.fills[0].commissionAsset}).`);
 
+            setTimeout(
+                await this.makeProfit,
+                60000 * this.trade.timeToSell!,
+                price
+            );
         }
     };
+
+    private async makeProfit(buyPrice: number) {
+        const pairSymbol: string = `${this.trade.coin}${this.trade.pairCoin}`;
+        const price: string = await this.getCurrentPairPrice();
+
+        const accountInfo: Account = await this.binanceClient!.accountInfo();
+        const pairCoinBalance: AssetBalance | undefined = accountInfo.balances.find((asset) => asset.asset === this.trade.coin);
+
+        if (!pairCoinBalance) {
+            console.error(`Insufficient ${this.trade.coin} balance.`);
+            return;
+        }
+
+        const sellAmount: number = parseInt(pairCoinBalance.free);
+
+        const order: Order = await this.binanceClient!.order({
+            symbol: pairSymbol,
+            side: OrderSide.SELL,
+            quantity: sellAmount.toString(),
+            type: OrderType.MARKET,
+            recvWindow: 60000,
+        });
+
+        if (order && order.fills) {
+            let price: number = 0;
+            let quantity: number = 0;
+            let commission: number = 0;
+
+            order.fills.forEach((fill: OrderFill) => {
+                price += parseFloat(fill.price);
+                quantity += parseFloat(fill.qty);
+                commission += parseFloat(fill.commission);
+            });
+
+            const avgPrice: number = price / order.fills.length;
+            const profit: string = (((price - buyPrice) / buyPrice) * 100).toFixed(2);
+
+            console.log(`Sold: ${order.origQty} ${order.symbol}. \n\n
+            Avg.  price ${avgPrice} (${quantity} ${order.symbol}). \n\n
+            Profit: ${profit}%. \n\n
+            Commission fee:${commission} (${order.fills[0].commissionAsset}).`);
+
+        }
+    }
+
+    private async getCurrentPairPrice(): Promise<string> {
+        const pairSymbol: string = `${this.trade.coin}${this.trade.pairCoin}`;
+        const priceList: { [index: string]: string } = await this.binanceClient!.prices({ symbol: pairSymbol });
+        return priceList[pairSymbol];
+    }
 
     private createTwitterClient(): Twit {
         if (!process.env.TWIITER_CONSUMER_KEY) {
